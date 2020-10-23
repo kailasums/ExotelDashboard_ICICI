9eab8139a176af2b591972ef21db03ddaf521819
krushna <batekrushna@gmail.com>
2020-10-23 14:50:48 +0530

    auth check

--- app/Http/Middleware/AuthBasic.php
+++ app/Http/Middleware/AuthBasic.php
@@ -16,6 +16,17 @@ class AuthBasic
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        header('WWW-Authenticate: Basic realm="My Realm"');
+        if (!isset($_SERVER['PHP_AUTH_USER'])) {    
+            header('HTTP/1.0 401 Unauthorized');
+            echo json_encode(['error' => 'auth fail']);
+            exit();
+        } else {
+            if($_SERVER['PHP_AUTH_USER'] === env("sid") && $_SERVER['PHP_AUTH_PW'] === env("sidToken")){
+                return $next($request);
+            }else{
+                echo  json_encode(['error' => 'auth fail']);exit();
+            } 
+        }
     }
 }
